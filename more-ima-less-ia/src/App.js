83909e8318a6c3b91b508903247b350a4fc02185
Michael <michael.mena@gmail.com>
2026-07-06 16:47:36 -0400

    Create initial site

---
+++ more-ima-less-ia/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your JSX code here */}
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
